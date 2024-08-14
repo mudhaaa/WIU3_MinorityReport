1a6b7ae710bbdaae11226d2204eae964fd95ca76
mudhaaa <129643813+mudhaaa@users.noreply.github.com>
2024-08-14 11:21:51 +0800

    Added character movement and colliders to map

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
+
     // Start is called before the first frame update
     Vector2 Direction = Vector2.zero;
     Rigidbody2D rb;
 
     void Start()
     {
+        moveSpeed = 5f;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Direction = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, Input.GetAxisRaw("Vertical") * moveSpeed);
     }
     private void FixedUpdate()
     {
