5bb5e0ddc27cde790a37bfdbcc54f3e4e2e241fc
reykenj <230343s@mymail.nyp.edu.sg>
2024-08-15 17:22:36 +0800

    PopLockStuffLol

--- Assets/IOMiniGame.cs
+++ Assets/IOMiniGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class IODrawer : MonoBehaviour, InteractableObject
+public class IOMiniGame : MonoBehaviour, InteractableObject
 {
     public BackgroundTransparencyAnim BlackBackground;
     public DialogSystem dialogSystem;
