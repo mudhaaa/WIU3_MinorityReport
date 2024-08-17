ba60cc75a95a7fd84d5bea7126b81983ff951d69
Jaydon <135347889+JaydonLeeEnjie@users.noreply.github.com>
2024-08-18 00:59:20 +0800

    clean mirror

---
+++ Assets/Scripts/Clean Mirror.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanMirror : MonoBehaviour
+{
+    public bool FinishGame = false;
+    public GameObject MainGame;
+    public GameObject MiniGame;
+    public BackgroundTransparencyAnim BlackBackground;
+    public List<GameObject> objectsToRandomize; // List of GameObjects to randomize
+
+    private void Start()
+    {
+            RandomizeObjects();
+    }
+
+    private void RandomizeObjects()
+    {
+        foreach (GameObject obj in objectsToRandomize)
+        {
+            // Add a random value to the x position
+            float randomX = Random.Range(-1.2f, 1.2f);
+            obj.transform.position = new Vector3(obj.transform.position.x + randomX, obj.transform.position.y, obj.transform.position.z);
+
+            // Add a random value to the y position
+            float randomY = Random.Range(-1.623f, 1.542f);
+            obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y + randomY, obj.transform.position.z);
+
+            // Randomize scale
+            float randomScale = Random.Range(0.07f, 0.16f);
+            obj.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D hit = Physics2D.OverlapPoint(mousePosition);
+
+            if (hit != null)
+            {
+                if (hit.gameObject.layer == LayerMask.NameToLayer("Dust"))
+                {
+                    hit.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        bool allClothesInactive = true;
+        foreach (GameObject dust in objectsToRandomize)
+        {
+            if (dust.activeInHierarchy)
+            {
+                allClothesInactive = false;
+                break;
+            }
+        }
+
+        if (allClothesInactive)
+        {
+            FinishGame = true;
+        }
+
+        if (FinishGame)
+        {
+            if (BlackBackground.CoroutineRunning == false)
+            {
+                FinishGame = true;
+                BlackBackground.StartCoroutine(BlackBackground.AppearAnim(1.0f, 0.01f, 0.25f));
+            }
+            if (BlackBackground.canvasgroup.alpha >= 1.0f)
+            {
+                FinishGame = false;
+                MiniGame.SetActive(false);
+                MainGame.SetActive(true);
+            }
+        }
+    }
+}
