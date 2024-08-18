439a8a10e8f4164409c3d56f383f9e37fdc7588e
mudhaaa <129643813+mudhaaa@users.noreply.github.com>
2024-08-18 16:23:38 +0800

    Added start of day dialogue

--- Assets/DayManager.cs
+++ Assets/DayManager.cs
@@ -4,24 +4,54 @@ using UnityEngine;
 
 public class DayManager : MonoBehaviour
 {
-    [SerializeField] float DayMaxMin = 8;
+    [SerializeField] float DayMaxMinutes = 8;
     [SerializeField] float TimeOfCurrentDay = 0;
-    int DayCount = 0;
+    [SerializeField] private DialogSystem dialogSystem;
+
+    public int currDay = 0;
+
+    string[] dialogueMPaths =  { "Assets/Dialog/MaleDay1Start.txt", "Assets/Dialog/MaleDay2Start.txt", "Assets/Dialog/MaleDay3Start.txt", "Assets/Dialog/MaleDay4Start.txt", "Assets/Dialog/MaleDay5Start.txt" };
+    string[] dialogueFPaths = { "Assets/Dialog/FemaleDay1Start.txt", "Assets/Dialog/FemaleDay2Start.txt", "Assets/Dialog/FemaleDay3Start.txt", "Assets/Dialog/FemaleDay4Start.txt", "Assets/Dialog/FemaleDay5Start.txt" };
+
+
+    bool timeStopped;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetTodayDialogue();
+        StartCoroutine(PlayStartDialogue());
     }
 
     // Update is called once per frame
     void Update()
     {
-        TimeOfCurrentDay += Time.deltaTime;
-
-        if (TimeOfCurrentDay > DayMaxMin * 60)
+        if (!timeStopped && TimeOfCurrentDay > DayMaxMinutes * 60)
         {
-            TimeOfCurrentDay = 0;
-            DayCount++;
+            currDay++;
+            StartCoroutine(PlayStartDialogue());
         }
+        if (!timeStopped && TimeOfCurrentDay <= DayMaxMinutes * 60)
+        {
+            TimeOfCurrentDay += Time.deltaTime;
+        }
+    }
+
+    void SetTodayDialogue()
+    {
+        dialogSystem.FilePath = dialogueMPaths[currDay];
+    }
+
+    IEnumerator PlayStartDialogue()
+    {
+        SetTodayDialogue();
+        dialogSystem.StartNewDialogues();
+        timeStopped = true;
+
+        yield return new WaitUntil(() => dialogSystem.IsDialogueEnded);
+
+        timeStopped = false;
+        TimeOfCurrentDay = 0;
+
     }
 }
---
+++ Assets/Scripts/DayStartDialogueUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayStartDialogueUI : MonoBehaviour
+{
+    [SerializeField] private DialogSystem dialogSystem;
+    [SerializeField] private DayManager dayMngr;
+    private int currDay;
+
+    void Start()
+    {
+        currDay = dayMngr.currDay;
+        SetTodayDialogue();
+        dialogSystem.StartNewDialogues();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetTodayDialogue()
+    {
+        if(currDay == 0)
+        {
+            dialogSystem.FilePath = "Assets/Dialog/Day1Start.txt";
+        }
+        if (currDay == 1)
+        {
+            dialogSystem.FilePath = "Assets/Dialog/Day2Start.txt";
+        }
+        if (currDay == 2)
+        {
+            dialogSystem.FilePath = "Assets/Dialog/Day3Start.txt";
+        }
+        if (currDay == 3)
+        {
+            dialogSystem.FilePath = "Assets/Dialog/Day4Start.txt";
+        }
+        if (currDay == 4)
+        {
+            dialogSystem.FilePath = "Assets/Dialog/Day5Start.txt";
+        }
+    }
+}
